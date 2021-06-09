aead2c21fc13df45368c641e74a3d0d8281b5e34
courtney l <clandau@learnics.com>
2021-06-09 08:12:40 -0400

    middleware to parse token working

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
 const app = require("express")();
+const cors = require("cors");
+const admin = require("firebase-admin");
 const httpServer = require("http").createServer(app);
+
+admin.initializeApp();
+
+app.use(cors());
+// Decodes the Firebase JSON Web Token
+app.use(decodeIDToken);
+
+/**
+ * Decodes the JSON Web Token sent via the frontend app
+ * Makes the currentUser (firebase) data available on the body.
+ */
+async function decodeIDToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      req['currentUser'] = decodedToken;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  next();
+}
+
+app.get("/api/newUser", (req, res) => {
+  const user = req["currentUser"];
+  console.log(user);
+  if (!user) res.status(403).send("Not logged in.");
+  else res.status(200).send({ message: "heeeey"});
+})
+
 const options = {
   cors: {
     origin: "http://localhost:8080",
