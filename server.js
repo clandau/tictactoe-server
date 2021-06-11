64395b08cd681ae71cd19a16ae87dbe40188e149
courtney l <clandau@learnics.com>
2021-06-11 17:09:21 -0400

    remove cors origin

--- server.js
+++ server.js
@@ -13,12 +13,7 @@ const availableRooms = [];
 const rooms = {};
 const state = {};
 
-var corsOptions = {
-  origin: 'https://tic-tac-toe-dfe95.web.app/*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(decodeIDToken);
 
 /**
